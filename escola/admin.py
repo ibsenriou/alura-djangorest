7cf0bfd72758637b71a08e17356068a4fd0cd946
ibsenriou <ibsenriou@hotmail.com>
2021-08-31 14:13:07 -0300

    first commit

---
+++ escola/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from escola.models import Aluno, Curso
+
+
+@admin.register(Aluno)
+class Alunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+
+@admin.register(Curso)
+class Cursos(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
+
+
+
